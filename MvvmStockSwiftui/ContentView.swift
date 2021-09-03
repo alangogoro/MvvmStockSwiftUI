329cf2c3a857fd599d77dc054a234ed0b028f347
alangogoro <cayne9160@gmail.com>
2021-09-03 13:00:35 +0800

    Finish user interface setting

--- MvvmStockSwiftui/ContentView.swift
+++ MvvmStockSwiftui/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchTerm: String = ""
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = UIColor.black
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        UITableView.appearance().backgroundColor = UIColor.black
+        UITableViewCell.appearance().backgroundColor = UIColor.black
+    }
+    
     var body: some View {
-        Text("Hello, world!".uppercased())
-            .padding()
+        NavigationView {
+            ZStack(alignment: .leading) {
+                Color.black
+                
+                Text("September 3 2021")
+                    .font(.custom("Arial",
+                                  size: 32))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.gray)
+                    .padding(EdgeInsets(top: 0,
+                                        leading: 20,
+                                        bottom: 0,
+                                        trailing: 0))
+                    .offset(y: -350)
+                    // .offset(y: -400)
+                
+                SearchView(searchTerm: $searchTerm)
+                    .offset(y: -300)
+            }
+            .navigationTitle("Stocks")
+        }.edgesIgnoringSafeArea(Edge.Set(.bottom))
     }
 }
 
